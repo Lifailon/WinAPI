4b3d58bace19667ca1b1085148008c5b533ef50e
Lifailon <deadepicboss@gmail.com>
2024-01-29 23:50:03 +0300

    Added check port

--- WinAPI/Process/winapi-process-start.ps1
+++ WinAPI/Process/winapi-process-start.ps1
@@ -1,3 +1,14 @@
-$path = "$home\Documents\WinAPI"
-$proc_id = $(Start-Process pwsh -ArgumentList "-File $path\winapi.ps1" -Verb RunAs -WindowStyle Hidden -PassThru).id
-$proc_id > "$path\process_id.txt"
+$winapi_path     = "$home\Documents\WInAPI"
+$ini_path        = "$winapi_path\winapi.ini"
+$Log_Path        = "$winapi_path\winapi.log"
+$ini = Get-Content $ini_path
+$port = $($ini | ConvertFrom-StringData).port
+$listen = Get-NetTCPConnection -LocalPort $port -State Listen -ErrorAction Ignore
+if ($listen) {
+    $date = Get-Date -Format "dd.MM.yyyy hh:mm:ss"
+    "$date Port $port is already in use" | Out-File $Log_Path -Encoding utf8 -Append    
+} 
+else {
+    $proc_id = $(Start-Process pwsh -ArgumentList "-File $winapi_path\winapi.ps1" -Verb RunAs -WindowStyle Hidden -PassThru).id
+    $proc_id > "$winapi_path\process_id.txt"
+}
