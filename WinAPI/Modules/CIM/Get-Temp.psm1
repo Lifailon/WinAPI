f033d4ee821bbdf736914471b4707ef37ad3e782
Lifailon <deadepicboss@gmail.com>
2024-01-29 11:41:53 +0300

    Update 0.4.1. Added test modules

--- WinAPI/Modules/CIM/Get-Temp.psm1
+++
@@ -1,6 +0,0 @@
-Get-CimInstance CIM_TemperatureSensor
-Get-CimInstance Win32_TemperatureProbe
-Get-CimInstance Win32_PerfFormattedData_Counters_ThermalZoneInformation
-Get-CimInstance MSAcpi_ThermalZoneTemperature -Namespace root/WMI
-Get-CimInstance CIM_Fan
-Get-CimInstance CIM_CoolingDevice
